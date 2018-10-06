1dc2820617e497340584470b103c21a611d96d3e
iVictor-Git <Victor.n.tran.social@outlook.com>
2018-10-06 00:59:28 -0400

    add a way to grab single bucketlist

--- src/data/api.js
+++ src/data/api.js
@@ -1,9 +1,25 @@
-import Data from './_Data'
+import Data from './_Data';
 
-export const getBucketLists = () => new Promise((resolve, reject) => {
-  const probability = Math.random() * 100;
-  const waitTime = Math.random() * 2000
-  setTimeout(() => {
-    probability < 10 ? reject('Something went wrong') : resolve(Data)
-  }, waitTime)
-})
+export const getBucketLists = () =>
+  new Promise((resolve, reject) => {
+    const probability = Math.random() * 100;
+    const waitTime = Math.random() * 2000;
+    setTimeout(() => {
+      probability < 10 ? reject('Something went wrong') : resolve(Data);
+    }, waitTime);
+  });
+
+export const getBucketList = id =>
+  new Promise((resolve, reject) => {
+    const probability = Math.random() * 100;
+    const waitTime = Math.random() * 2000;
+
+    const data =
+      probability < 10
+        ? 'Something went wrong'
+        : Data.filter(item => item.id === id);
+
+    setTimeout(() => {
+      probability < 10 || !data ? reject(data) : resolve(data);
+    }, waitTime);
+  });
