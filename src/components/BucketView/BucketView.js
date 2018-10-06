3d44f3c5d68cc57e9e19f94a25b10a1ac10846be
iVictor-Git <Victor.n.tran.social@outlook.com>
2018-10-06 01:56:03 -0400

    nothing important

--- src/components/BucketView/BucketView.js
+++ src/components/BucketView/BucketView.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import BucketListLoader from '../BucketListLoader/BucketListLoader';
-import BucketDetails from '../BucketListPreview';
+import BucketDetails from '../BucketListPreview/BucketListPreview';
 
 export default props => {
+  console.log(props);
   const FullBucketDetails = BucketListLoader(BucketDetails);
+
+  return <FullBucketDetails />;
 };
