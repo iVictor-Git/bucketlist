ab47829f233fa75e3b9a9d322ceb8bcc61757717
iVictor-Git <Victor.n.tran.social@outlook.com>
2018-10-05 22:45:25 -0400

    added an extra prop field

--- src/components/UI/Icon/Icon.js
+++ src/components/UI/Icon/Icon.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default ({ source, alt, className, style }) => {
+export default ({ source, alt, className, style, divStyle }) => {
   return (
-    <div className={`${className} Logo`}>
+    <div className={`${className} Logo`} style={divStyle}>
       <img src={source} alt={alt} style={style} />
     </div>
   );
 };
-
