b09cc8e466ab52e7d641ed484387f40145d8be33
Victor <victor.n.tran@outlook.com>
2018-10-05 20:36:11 -0400

    incorporate bigger components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import TitleBar from './components/TitleBar/TitleBar';
+import AppTitleBar from './components/AppTitleBar/AppTitleBar';
+import List from './components/UI/List/List';
+import withBucketList from './components/BucketList/BucketList';
 
 // mobile first approach
 
 class App extends Component {
   render() {
+    const BucketList = withBucketList(List);
+
     return (
       <div className="App">
-        <TitleBar />
+        <AppTitleBar />
+        <BucketList className='BucketList'/>
       </div>
     );
   }
