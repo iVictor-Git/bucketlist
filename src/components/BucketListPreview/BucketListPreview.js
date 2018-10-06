5a133e1b763165d007ec3a176a8e1437a5b5dc6f
Victor tran <victor.n.tran.social@outlook.com>
2018-10-05 20:48:15 -0400

    fix styling and limit preview characters to be 160

--- src/components/BucketListPreview/BucketListPreview.js
+++ src/components/BucketListPreview/BucketListPreview.js
@@ -8,20 +8,23 @@ import Text from '../UI/Text/Text';
 
 const transformText = (text = '', max) => {
   return text.slice(0, max).concat('...');
-}
+};
 
 const evalulateText = (text = '', max) => {
-  return text.length < max ? text : transformText(text, max)
-}
+  return text.length < max ? text : transformText(text, max);
+};
 
-export default ({title = 'Some title', hearts = 0, text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit,"}) => {
-
-  const transformedText = evalulateText(text, 200);
+export default ({
+  title = 'Some title',
+  hearts = 0,
+  text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit,',
+}) => {
+  const transformedText = evalulateText(text, 160);
 
   return (
     <section className="BucketListPreview">
-      <BucketListTitleBar title={title} hearts={hearts}/>
-      <Text text={transformedText} className='BucketListTextPreview'/>
+      <BucketListTitleBar title={title} hearts={hearts} />
+      <Text text={transformedText} className="BucketListTextPreview" />
     </section>
   );
 };
