6088ad26095af87f98dba95d316fa878dcff6e14
iVictor-Git <Victor.n.tran.social@outlook.com>
2018-10-05 22:46:01 -0400

    refactor text to differentiate between k, m, b, t

--- src/components/UI/Text/Text.js
+++ src/components/UI/Text/Text.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
 const transformText = text => {
-  const suffix = ['', 'K', 'M', 'B', 'T']
+  const suffix = ['', 'K', 'M', 'B', 'T'];
   const length = text.toString().length;
   const suffixNumber = Math.floor(length / 3);
-  const newNumber = (Number.parseInt(text) / Math.pow(100, suffixNumber)).toFixed(1).toString()
-  return `${newNumber.length > 4 ? newNumber.slice(0, 3) : newNumber }${suffix[suffixNumber]}`
-}
-
+  const newNumber = (Number.parseInt(text) / Math.pow(100, suffixNumber))
+    .toFixed(1)
+    .toString();
+  return `${newNumber.length > 4 ? newNumber.slice(0, 3) : newNumber}${
+    suffix[suffixNumber]
+  }`;
+};
 
 const evaluateText = text => {
-  return typeof text === 'string' ? text : transformText(text)
-
-}
+  const Text = text;
+  return typeof text === 'string' ? (
+    text
+  ) : typeof text === 'number' ? (
+    transformText(text)
+  ) : (
+    <Text />
+  );
+};
 
 export default ({ className, style, text }) => {
-
   return (
-    <p className={className} style={style}>
+    <div className={className} style={style}>
       {evaluateText(text)}
-    </p>
+    </div>
   );
 };
