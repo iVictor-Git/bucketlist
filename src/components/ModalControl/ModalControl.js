b95adaf3831ebe6f5328755c35ffe56f2da2b071
Victor <victor.n.tran@outlook.com>
2018-10-07 22:45:46 -0400

    create a modal controller that updates the state to show modal

---
+++ src/components/ModalControl/ModalControl.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Modal from '../UI/Modal/Modal';
+import AddBucketForm from '../AddBucketForm/AddBucketForm';
+import AddBucketListItemButton from '../AddBucketListButton/AddBucketListItemButton';
+import Backdrop from '../UI/Backdrop/Backdrop';
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+  }
+
+  toggleModal = () => {
+    this.setState((prevState, _) => {
+      return {
+        show: !prevState.show,
+      };
+    });
+  };
+
+  render() {
+    const { show } = this.state;
+    const modalClassName = 'Modal '.concat(show ? 'Show' : 'Hide');
+    return (
+      <section>
+        <AddBucketListItemButton toggleModal={this.toggleModal} />
+        <Backdrop show={show} toggleModal={this.toggleModal} />
+        <Modal className={modalClassName} title="Add a Bucketlist">
+          <AddBucketForm
+            className="AddBucketForm"
+            titlePlaceholder="Title"
+            textPlaceholder="Description"
+          />
+        </Modal>
+      </section>
+    );
+  }
+}
