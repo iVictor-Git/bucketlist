25fc261ffe535732240928cefe29cc8e1bc13ac5
Victor <victor.n.tran@outlook.com>
2018-10-07 22:45:20 -0400

    Gave named export and removed fragment portion to allow greater control through css

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import AddBucketListItemButton from '../AddBucketListButton/AddBucketListItemButton';
+import React, { Component } from 'react';
 import withBucketList from '../BucketList/BucketList';
 import List from '../UI/List/List';
 
-export default () => {
-  const BucketList = withBucketList(List);
+import './HomeView.css';
+import ModalControl from '../ModalControl/ModalControl';
 
-  return (
-    <React.Fragment>
-      <BucketList className="BucketList" />
-      <AddBucketListItemButton />
-    </React.Fragment>
-  );
-};
+class HomeView extends Component {
+  
+  render() {
+    const BucketList = withBucketList(List);
+
+    return (
+      <section className='HomeView'>
+        <BucketList className="BucketList" />
+        <ModalControl />
+      </section>
+    );
+  }
+}
+
+export default HomeView
