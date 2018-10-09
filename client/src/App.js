6bdbaf2c22d1983dd76fd126863fa78859c85bc8
Victor <victor.n.tran@outlook.com>
2018-10-09 12:13:34 -0400

    add apollo client and provider

--- client/src/App.js
+++ client/src/App.js
@@ -11,20 +11,29 @@ import HomeView from './components/HomeView/HomeView';
 import { Route, Switch } from 'react-router-dom';
 import BucketView from './components/BucketView/BucketView';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppTitleBar />
-        <View className="View">
-          <Switch>
-            <Route exact path="/" component={HomeView} />
-            <Route path="/bucket" component={BucketView} />
-            <Route component={() => <h1>Not Found</h1>} />
-          </Switch>
-        </View>
-        <Navigation />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <AppTitleBar />
+          <View className="View">
+            <Switch>
+              <Route exact path="/" component={HomeView} />
+              <Route path="/bucket" component={BucketView} />
+              <Route component={() => <h1>Not Found</h1>} />
+            </Switch>
+          </View>
+          <Navigation />
+        </div>
+      </ApolloProvider>
     );
   }
 }
