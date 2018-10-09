306aa970673ac674d677700cb6c3fe68c1329133
Victor <victor.n.tran@outlook.com>
2018-10-08 22:44:48 -0400

    add 2 additional props, hearts, and userIds

--- server/models/BucketList/bucketlist.js
+++ server/models/BucketList/bucketlist.js
@@ -4,7 +4,9 @@ const BucketSchema = new mongoose.Schema({
   title: {type: String, required: true},
   text: {type: String, required: true},
   dateCreated: {type: Date, default: Date.now()},
-  ownerId: {type: String, required: true}
+  ownerId: {type: String, required: true},
+  hearts: {type: Number, default: 0},
+  userIds: {type: [Number]}
 })
 
 module.exports = mongoose.model('BucketList', BucketSchema);
