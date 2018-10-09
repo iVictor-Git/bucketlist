639f0949f0b70d2cec51aa72fc84d1fb5775f430
Victor <victor.n.tran@outlook.com>
2018-10-09 17:49:39 -0400

    create full page leader (lazily), refactor later

---
+++ client/src/components/FullPageLoader/FullPageLoader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Loader from '../UI/Loader/Loader';
+
+const FullPageLoader = () => (
+  <React.Fragment>
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+    <Loader className="BucketListPreview Loading" />
+  </React.Fragment>
+);
+
+export default FullPageLoader;
