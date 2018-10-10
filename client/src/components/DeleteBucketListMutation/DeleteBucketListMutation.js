05c9e0fb226dae72e9a27c68604afc8d6fd25bf4
iVictor-Git <Victor.n.tran.social@outlook.com>
2018-10-10 12:01:14 -0400

    refactor Mutation component, TODO: get cache working

--- client/src/components/DeleteBucketListMutation/DeleteBucketListMutation.js
+++ client/src/components/DeleteBucketListMutation/DeleteBucketListMutation.js
@@ -2,24 +2,40 @@ import React from 'react';
 import { Mutation } from 'react-apollo';
 import { DELETE_BUCKETLIST } from '../../graphql/queries/';
 import Text from '../UI/Text/Text';
-import DeleteModalControl from '../DeleteModalControl/DeleteModalControl'
+import DeleteModalControl from '../DeleteModalControl/DeleteModalControl';
 
-const DeleteBucketListMutation = ({id}) => {
-
-  const handleClick = (deleteBucketList) => {
-    deleteBucketList({variables: {
-      id: id
-    }})
-  }
+const DeleteBucketListMutation = ({ id }) => {
+  const handleClick = deleteBucketList => {
+    deleteBucketList({
+      variables: {
+        id: id,
+      },
+    });
+  };
 
   return (
-    <Mutation mutation={DELETE_BUCKETLIST}>
-      {(deleteBucketList, { data, loading, error }) => (
-        <React.Fragment>
-          <Text text={'Delete'} className="small delete button Detail" onClick={handleClick}/>
-          {!loading && <DeleteModalControl show='true' />}
-        </React.Fragment>
-      )}
+    <Mutation
+      mutation={DELETE_BUCKETLIST}
+      update={(cache, { deleteBucketList }) => {
+        // TODO: need to fix cache to reflect changes made to server
+        console.log(cache);
+        cache.writeQuery({
+          query: { DELETE_BUCKETLIST },
+          data: { bucketlists: deleteBucketList },
+        });
+      }}>
+      {(deleteBucketList, { data, loading, error, called }) => {
+        return (
+          <React.Fragment>
+            <Text
+              text={'Delete'}
+              className="small delete button Detail"
+              onClick={() => handleClick(deleteBucketList)}
+            />
+            {!loading && called && <DeleteModalControl show="true" />}
+          </React.Fragment>
+        );
+      }}
     </Mutation>
   );
 };
