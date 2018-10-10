f523f6976584ae38ab35cb5598fdd846b1acce5a
iVictor-Git <Victor.n.tran.social@outlook.com>
2018-10-10 12:05:59 -0400

    instead of returning a single bucketlist, returns a new list of current bucketlists

--- server/schema/mutations/bucketlist/delete_bucket_list.js
+++ server/schema/mutations/bucketlist/delete_bucket_list.js
@@ -1,7 +1,17 @@
 module.exports = {
   Mutation: {
-    deleteBucketList: (_, { id }, { models: { BucketList } }) => {
-      return BucketList.findByIdAndRemove(id);
+    deleteBucketList: async (_, { id }, { models: { BucketList } }) => {
+      const query = BucketList.findByIdAndDelete(id);
+      query.exec((err, result) => {
+        if (err) return Error('Something went wrong with deleting document');
+      });
+
+      const bucketlists = await BucketList.find();
+
+      const newBucketlist = bucketlists.filter(bucketlist => {
+        return bucketlist.id !== id;
+      });
+      return newBucketlist;
     },
   },
 };
